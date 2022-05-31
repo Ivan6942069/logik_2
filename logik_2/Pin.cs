c0c0c928807248f7a0f92a09ec09b8fa2fdf7da9
sorban.ivan <sorban.ivan@D20-06>
2022-05-31 11:08:21 +0200

    Přidejte soubory projektu.

---
+++ logik_2/Pin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace logik_2
+{
+    public class Pin
+    {
+        public Color Color { get; set; }
+    }
+
+    public static class Pincolor
+    {
+        public static Color Color1 = Colors.Red;
+        public static Color Color2 = Colors.Green;
+        public static Color Color3 = Colors.Blue;
+        public static Color Color4 = Colors.Orange;
+        public static Color Color5 = Colors.Yellow;
+        public static Color Color6 = Colors.Purple;
+    }
+}
