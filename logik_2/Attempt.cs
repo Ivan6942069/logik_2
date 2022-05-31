87b17a13b2f0048a2bf5f5c077d51f155ef8cbcb
sorban.ivan <sorban.ivan@D20-06>
2022-05-31 11:37:14 +0200

    a

---
+++ logik_2/Attempt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logik_2
+{
+    public class Attempt
+    {
+        public Pin[] Pins { get; internal set; }
+
+        public Attempt()
+        {
+            Pins = new Pin[4];
+        }
+
+        public List<bool> Evaluate(Pin[] pattern)
+        {
+            List<bool> result = new List<bool>();
+            bool[] flag = new bool[4];
+            int i = 0;
+            
+            return new List<bool>();
+        }
+    }
+}
